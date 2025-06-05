6d83e50951d25241365979672463561ddade82be
patrick.dev.br <patrickribeiro.rb@gmail.com>
2025-06-05 04:03:35 -0300

    chore: rest_api application struct

---
+++ apps/content-service/cmd/rest_api/application.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/joho/godotenv"
+	"github.com/patrickdevbr-portfolio/cms/apps/content-service/internal/application/services"
+	"github.com/patrickdevbr-portfolio/cms/apps/content-service/internal/infra/db/mongodb"
+	"github.com/patrickdevbr-portfolio/cms/apps/content-service/internal/infra/rest"
+	"github.com/patrickdevbr-portfolio/cms/libs/go-common/mongodatabase"
+)
+
+type application struct {
+	config config
+}
+
+type config struct {
+	addr string
+}
+
+func (app *application) run() error {
+	godotenv.Load()
+
+	mux := http.NewServeMux()
+
+	// oidcProvider, err := auth.NewOIDCProvider()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// authMiddleware := auth.NewMiddleware(oidcProvider)
+
+	mongoClient, err := mongodatabase.Connect(context.Background())
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	rest.NewPageRest(mux, &services.PageServiceImpl{
+		PageRepository: &mongodb.PageRepository{
+			Client: mongoClient,
+		},
+	})
+
+	srv := &http.Server{
+		Addr:    app.config.addr,
+		Handler: mux,
+	}
+
+	return srv.ListenAndServe()
+}
--- apps/content-service/cmd/rest_api/main.go
+++ apps/content-service/cmd/rest_api/main.go
@@ -1,42 +1,13 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"net/http"
-
-	"github.com/joho/godotenv"
-	"github.com/patrickdevbr-portfolio/cms/apps/content-service/internal/application/services"
-	"github.com/patrickdevbr-portfolio/cms/apps/content-service/internal/infra/db/mongodb"
-	"github.com/patrickdevbr-portfolio/cms/apps/content-service/internal/infra/rest"
-	"github.com/patrickdevbr-portfolio/cms/libs/go-common/mongodatabase"
+	"log"
 )
 
 func main() {
-	godotenv.Load()
-	router := http.NewServeMux()
-
-	// oidcProvider, err := auth.NewOIDCProvider()
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// authMiddleware := auth.NewMiddleware(oidcProvider)
-
-	mongoClient, err := mongodatabase.Connect(context.Background())
-	if err != nil {
-		fmt.Println(err)
+	app := &application{
+		config: config{addr: ":8080"},
 	}
 
-	rest.NewPageRest(router, &services.PageServiceImpl{
-		PageRepository: &mongodb.PageRepository{
-			Client: mongoClient,
-		},
-	})
-
-	server := http.Server{
-		Addr:    ":8080",
-		Handler: router,
-	}
-
-	server.ListenAndServe()
+	log.Fatal(app.run())
 }
